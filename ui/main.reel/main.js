eaa8b8aaac08c354477bd252928ff842b0c62576
Mike Czepiel <mike@czepiel.net>
2014-05-21 01:55:52 -0700

    Remove wordpress component
    Wasn't really doing anything significant

--- ui/main.reel/main.js
+++ ui/main.reel/main.js
@@ -13,5 +13,29 @@ exports.Main = Component.specialize(/** @lends Main# */ {
         value: function Main() {
             this.super();
         }
+    },
+
+    templateDidLoad: {
+        value: function(firstTime) {
+            if (firstTime) {
+                this.addPathChangeListener("selectedPost", this, "handleSelection");
+            }
+        }
+    },
+
+    articlesController: {
+        value: null
+    },
+
+    handleSelection: {
+        value: function(post) {
+            if (!post) return;
+
+            window.setTimeout(function() {
+                var blog = document.getElementById("blog-post")
+                blog.style.opacity = 1;
+                blog.style.zIndex = 15;
+            }, 1200);
+        }
     }
 });
