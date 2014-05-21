1377a7ea179aeb300ab625fe71b307a701ef5eac
Mike Czepiel <mike@czepiel.net>
2014-05-21 16:35:46 -0700

    Manage more state in CSS to avoid timeout dance
    It might be less robust but it generally fares well and requires no
    imperative code.

--- ui/main.reel/main.js
+++ ui/main.reel/main.js
@@ -13,48 +13,5 @@ exports.Main = Component.specialize(/** @lends Main# */ {
         value: function Main() {
             this.super();
         }
-    },
-
-    templateDidLoad: {
-        value: function(firstTime) {
-            if (firstTime) {
-                this.addPathChangeListener("selectedPost", this, "revealPost");
-            }
-        }
-    },
-    
-    revealPost: {
-        value: function () {
-            
-            if (0 === this.templateObjects.articlesController.selection.length) {
-                return;
-            }
-            
-            window.setTimeout(function() {
-                var blog = document.getElementById("blog-post")
-                blog.style.opacity = 1;
-                blog.style.zIndex = 15;
-            }, 1200);
-        }
-    },
-    
-    hidePost: {
-        value: function() {
-            
-            if (this.templateObjects.articlesController.selection.length === 0) {
-                return;
-            }
-            
-            var blog = document.querySelector("#blog-post")
-            blog.style.opacity = 0;
-
-            document.querySelector(".js-card.selected").classList.remove("selected");
-
-            var self = this;
-            window.setTimeout(function() {
-                blog.style.zIndex = 0;
-                self.templateObjects.articlesController.clearSelection();
-            }, 1000);
-        }
     }
 });
